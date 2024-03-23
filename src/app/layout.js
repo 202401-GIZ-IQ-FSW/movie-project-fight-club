e20d3584ac2ae4739b98a1c7da52aced579b2af9
MUSTAFA MAZIN AHMED <mustafa.hamedi.2001@gmail.com>
2024-03-23 10:46:54 +0300

    adding search box

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import SearchBox from "@/components/SearchBox";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,8 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
-      <Header/>
+      <body className={inter.className}>
+        {children}
+        <Header />
+        <SearchBox />
       </body>
     </html>
   );
