741ae30d7a921a91f0bc35cd1d4fcbc7ed6aef1a
Harivan-T <Harivantahir@gmial.com>
2024-03-23 13:34:52 +0300

    completed the nav bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
-
+import Header from "@/components/Header";
 export const metadata = {
   title: "Movie Project",
   description: "Movie database clone",
@@ -10,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+<Header/>
       <body className={inter.className}>{children}</body>
     </html>
   );
