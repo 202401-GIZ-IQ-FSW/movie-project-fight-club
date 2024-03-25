0acbe14d6f208889b65dc0aae1e18ea82b1548ac
PayamRasho <payamrfattah01@gmail.com>
2024-03-25 18:00:37 +0300

    feat: footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Fight Club",
+  title: "Movie Project",
   description: "Movie database clone",
 };
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer/>
       </body>
     </html>
   );
